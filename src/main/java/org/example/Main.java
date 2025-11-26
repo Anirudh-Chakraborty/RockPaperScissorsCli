a57e8c73645ba2e1a64d0f6ec8ba6f50018aa123
anirudhchakraborty <preetiujjal@gmail.com>
2025-11-27 02:42:04 +0530

    Menu Method Created and called from the main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,24 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    static Main caller = new Main();
 
 
     public static void main(String[] args) {
-
+        caller.menu();
     }
+
+    void menu(){
+        System.out.println("Welcome to the RPS CLi Game");
+        System.out.println("Make you choice and the computer will play against you");
+        System.out.println("You will have to chose one of the following using numbers ");
+        System.out.println("1. Rock");
+        System.out.println("2. Scissors");
+        System.out.println("3. Paper");
+
+        System.out.println("Make a Choice 1/2/3");
+    }
+
 
 
 
